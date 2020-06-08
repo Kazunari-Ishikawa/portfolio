69665cd67d550c46343df3d74a82692b6daac34d
kazunari-ishikawa <zunari.computercity@gmail.com>
2020-06-09 05:52:07 +0900

    Header.vueの追加

---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <header class="l-header">
+    <a class="l-header__title" href="index.html">
+      <h1>Portfolio</h1>
+    </a>
+    <nav class="l-nav">
+      <ul class="c-nav">
+        <li class="c-nav__item">
+          <a class="c-nav__link" href="#">TOP</a>
+        </li>
+        <li class="c-nav__item">
+          <a class="c-nav__link" href="#works">WORKS</a>
+        </li>
+        <li class="c-nav__item">
+          <a class="c-nav__link" href="#profile">PROFILE</a>
+        </li>
+      </ul>
+    </nav>
+
+    <!-- ハンバーガーメニュー -->
+    <div class="c-menuTrigger">
+      <span class="c-menuTrigger__bar"></span>
+      <span class="c-menuTrigger__bar"></span>
+      <span class="c-menuTrigger__bar"></span>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped>
+</style>
