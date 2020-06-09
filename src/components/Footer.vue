15edae370aa23f51f4a2aa3fcb65847d4a68346c
kazunari-ishikawa <zunari.computercity@gmail.com>
2020-06-09 15:40:23 +0900

    ハンバーガーメニューのJS実装、フッターのリンク設定

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,12 +3,12 @@
     <div class="l-footer__body">
       <ul class="l-footer__link">
         <li>
-          <a class="l-footer__item" href>
+          <a class="l-footer__item" href="https://twitter.com/kazusan2020" target="_blank">
             <i class="fab fa-twitter"></i>
           </a>
         </li>
         <li>
-          <a class="l-footer__item" href>
+          <a class="l-footer__item" href="https://github.com/Kazunari-Ishikawa" target="_blank">
             <i class="fab fa-github"></i>
           </a>
         </li>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,8 @@
     <a class="l-header__title" href="index.html">
       <h1>Portfolio</h1>
     </a>
-    <nav class="l-nav">
+    <nav class="l-nav js-header-toggle-target" :class="{isActive : toggleActive}">
+      <!-- PC用メニュー -->
       <ul class="c-nav">
         <li class="c-nav__item">
           <a class="c-nav__link" href="#">TOP</a>
@@ -15,13 +16,26 @@
           <a class="c-nav__link" href="#profile">PROFILE</a>
         </li>
       </ul>
+
+      <!-- スマホ・タブレット用メニュー -->
+      <ul class="c-spNav">
+        <li class="c-spNav__item">
+          <a class="c-spNav__link" href="#">TOP</a>
+        </li>
+        <li class="c-spNav__item">
+          <a class="c-spNav__link" href="#works">WORKS</a>
+        </li>
+        <li class="c-spNav__item">
+          <a class="c-spNav__link" href="#profile">PROFILE</a>
+        </li>
+      </ul>
     </nav>
 
     <!-- ハンバーガーメニュー -->
-    <div class="c-menuTrigger">
-      <span class="c-menuTrigger__bar"></span>
-      <span class="c-menuTrigger__bar"></span>
-      <span class="c-menuTrigger__bar"></span>
+    <div class="c-menuTrigger js-header-trigger" @click="toggleActive = !toggleActive">
+      <span class="c-menuTrigger__bar js-trigger-bar" :class="{isActive : toggleActive}"></span>
+      <span class="c-menuTrigger__bar js-trigger-bar" :class="{isActive : toggleActive}"></span>
+      <span class="c-menuTrigger__bar js-trigger-bar" :class="{isActive : toggleActive}"></span>
     </div>
   </header>
 </template>
@@ -31,7 +45,8 @@ export default {
   name: "Header",
   data() {
     return {
-      isActive: false
+      isActive: false,
+      toggleActive: false
     };
   },
   methods: {
