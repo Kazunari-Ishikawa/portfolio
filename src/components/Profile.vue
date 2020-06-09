8492fc4586c0e6056c81c5df71febc8bd0182045
kazunari-ishikawa <zunari.computercity@gmail.com>
2020-06-09 16:47:08 +0900

    Profile.vueのコンテンツ作成、フッターやheroのレスポンシブ、TDK設置

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -2,7 +2,12 @@
   <section id="profile" class="l-container">
     <h2 class="l-container__title">PROFILE</h2>
     <div class="l-container__body">
-      <p>自己紹介</p>
+      <p class="p-profile__text">1991年生まれ、愛知県名古屋市在住。</p>
+      <p class="p-profile__text">
+        鉄道車両メーカにてMATLABを使ったデータ解析や、制御・システム設計に従事。
+        <br />その後自動車関連のエンジニアリング会社にてECU設計に携わったのち、2019年9月退職。
+        <br />物づくりから、サービス・仕組みを自ら作りたいと思い、2019年の12月からオンラインスクール及び独学にてWEB系プログラミング（PHP, JavaScript）を勉強中。
+      </p>
     </div>
   </section>
 </template>
